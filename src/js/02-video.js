647751c54f47306ebba16e645950dea9748f18ec
IrynaHavryliuk <99551348+IrynaHavryliuk@users.noreply.github.com>
2022-10-23 20:48:37 +0300

    зміни

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,16 @@
-import '../css/common.css';
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
 
-const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const keyCurrentTime = 'videoplayer-current-time';
 
-const onPlay = function (data) {
-  const strigifyData = JSON.stringify(data);
-  localStorage.setItem(TIME_KEY, strigifyData);
-};
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function resumePlayback() {
-  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
-    return;
-  }
-  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
-  if (paused) {
-    player
-      .setCurrentTime(paused)
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'Error':
-            break;
-          default:
-            break;
-        }
-      });
-  }
+player.on('timeupdate', throttle(onSetTimeupdate, 500));
+function onSetTimeupdate(data) {
+  console.log('data.seconds: ', data.seconds);
+  localStorage.setItem(keyCurrentTime, data.seconds);
 }
-resumePlayback();
+
+if (localStorage.getItem(keyCurrentTime)) {
+  player.setCurrentTime(localStorage.getItem(keyCurrentTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,42 @@
-import '../css/common.css';
-import '../css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('input'),
-};
-const formData = {};
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('[name="email"]');
+const messageRef = document.querySelector('[name="message"]');
 
-populateTextarea();
+const FEEDBACK_KEY = 'feedback-form-state';
+let formData = {};
 
-refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+onFormInitial();
 
-refs.form.addEventListener('submit', e => {
-  e.preventDefault();
-  e.currentTarget.reset();
-  const objData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  localStorage.removeItem(STORAGE_KEY);
-});
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
 
-function onTextareaInput(e) {
-  formData[e.target.name] = e.target.value;
-  const stringifiedData = JSON.stringify(formData);
-  localStorage.setItem(STORAGE_KEY, stringifiedData);
+function onFormInput(evt) {
+
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
-function populateTextarea() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+function onFormInitial() {
+  const initialData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
 
-  if (savedMessage === null) {
-    //console.log(savedMessage);
+  if (!initialData) {
     return;
   }
-  refs.textarea.value = savedMessage['message'] || '';
-  refs.input.value = savedMessage['email'] || '';
+  if (initialData.email) {
+    emailRef.value = initialData.email;
+  }
+  if (initialData.message) {
+    messageRef.value = initialData.message;
+  }
+  formData = initialData;
 }
 
-
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log('formData: ', formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_KEY);
+  formData = {};
+}
